c765b2bc3a9960488fb7779c96def5536c95dc6b
Alan Ranciato <alanranciato@gmail.com>
2021-12-06 12:22:00 -0500

    added unit test file

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import Flask, request
 
 app = Flask(__name__)
 cloudName = os.getenv('CLOUD_LOCATION')
+__version__ = '0.1'
 
 @app.route("/", methods=["GET"])
 def homepage():
     if request.method == "GET":
-        return "Hello World from {0}".format(cloudName)
+        return "Hello World from {0} \n\n Version: {1}".format(cloudName, __version__)
 
 #PORT = int(os.environ.get("PORT", 8080))
 PORT = 8080
